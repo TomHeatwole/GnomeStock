8520c5ef008cff0a6ebe32b884e07ebbdb554f4c
Tom <tgh8xn@virginia.edu>
2017-11-26 15:28:28 -0500

    Skeleton files for admin functionality

---
+++ public/admin/admin.js
@@ -0,0 +1,31 @@
+var user;
+
+firebase.auth().onAuthStateChanged(function(u) {
+    if (u) {
+        user = u;
+        // $("h1").text("Hello, " + u.displayName + "!"); display names will need to be filled in later...
+        if (u.email != "tom@tom.tom")
+            window.location = "https://gnomestocks.com/";
+        displayLoadedPage();
+        start(u);
+    } else {
+        window.location = "https://gnomestocks.com/auth/auth.html";  
+    }
+});
+
+
+var signOut = function() {
+    firebase.auth().signOut().catch(function(e) {
+       alert(e.messge); 
+    });
+}
+
+var displayLoadedPage = function() {
+    document.getElementById("loading").style = "display: none";
+    document.getElementById("loaded").style = "display: lol";
+}
+
+var start = function(user) {
+
+}
+
