6d01c14b7cf79cc0fa8f678b43967ea68698f7fb
Tom <tgh8xn@virginia.edu>
2017-11-26 21:25:37 -0500

    Start/End functionality for admin page

--- public/admin/admin.js
+++ public/admin/admin.js
@@ -1,4 +1,11 @@
 var user;
+var weekIndex;
+var monthIndex;
+var weekly;
+var monthly;
+var marketOpen;
+var wLow;
+var wHigh;
 
 firebase.auth().onAuthStateChanged(function(u) {
     if (u) {
@@ -6,13 +13,41 @@ firebase.auth().onAuthStateChanged(function(u) {
         // $("h1").text("Hello, " + u.displayName + "!"); display names will need to be filled in later...
         if (u.email != "tom@tom.tom")
             window.location = "https://gnomestocks.com/";
-        displayLoadedPage();
+        getMasterValues();
         start(u);
     } else {
         window.location = "https://gnomestocks.com/auth/auth.html";  
     }
 });
 
+var getMasterValues = function() {
+    firebase.database().ref("master").once("value").then(function(master) {
+        weekIndex = master.val().week;
+        monthIndex = master.val().month;
+        weekly = master.val().weekly;
+        monthly = master.val().monthly;
+        marketOpen = master.val().market;
+        wLow = master.val().wLow;
+        wHigh = master.val().wHigh;
+        document.getElementById("startWeek").innerHTML = "Start Week " + (weekIndex + 1);
+        document.getElementById("endWeek").innerHTML = "End Week " + weekIndex;
+        document.getElementById("startMonth").innerHTML = "Start Month " + (monthIndex + 1);
+        document.getElementById("endMonth").innerHTML = "End Month " + monthIndex;
+        if (weekly)
+            document.getElementById("endWeek").style = "display: lol";
+        else
+            document.getElementById("startWeek").style = "display: lol";
+        if (monthly)
+            document.getElementById("endMonth").style = "display: lol";
+        else
+            document.getElementById("startMonth").style = "display: lol";
+        if (marketOpen)
+            document.getElementById("stopTrading").style = "display: lol";
+        else
+            document.getElementById("startTrading").style = "display: lol";
+        displayLoadedPage();
+    });
+}
 
 var signOut = function() {
     firebase.auth().signOut().catch(function(e) {
@@ -32,6 +67,47 @@ var start = function(user) {
 var home = function() {
     window.location = "https://gnomestocks.com/auth/auth.html";
 }
+
+var startWeek = function() {
+    firebase.database().ref("master/").update({
+        "week" : (weekIndex + 1),
+        "weekly" : true
+    }).then(function() {
+        alert("Now accepting evals for week " + (weekIndex + 1));
+        window.location.reload();
+    });
+}
+
+var endWeek = function() {
+    firebase.database().ref("master/").update({
+        "weekly" : false 
+    }).then(function() {
+        alert("No longer accepting evals for week " + weekIndex);
+        window.location.reload();
+    });
+}
+
+var startMonth = function() {
+    firebase.database().ref("master/").update({
+        "monthly" : true,
+        "month" : (monthIndex + 1),
+        "wLow" : wHigh,
+        "wHigh" : weekIndex
+    }).then(function() {
+        alert("Now accepting evals for month " + (monthIndex + 1));
+        window.location.reload();
+    });
+}
+
+var endMonth = function() {
+    firebase.database().ref("master/").update({
+        "monthly" : false 
+    }).then(function() {
+        alert("No longer accepting evals for month " + monthIndex);
+        window.location.reload();
+    });
+}
+
 
 var submit = function() {
     var perms = document.getElementsByTagName("input"); // weekly, monthly, trading
