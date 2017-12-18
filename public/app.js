fc38f9abfd26af096204a651e59f1f4b8c2464a0
tomheatwole <tgh8xn@virginia.edu>
2017-12-18 03:47:04 -0500

    Outline for "Your Portfolio" feature

--- public/app.js
+++ public/app.js
@@ -115,7 +115,24 @@ var displayLoadedPage = function() {
 }
 
 var start = function(user) {
-
+    var chart = new CanvasJS.Chart("chartContainer", {
+        data: [{
+            type: "pie",
+            startAngle: 240,
+            yValueFormatString: "##0.00\"%\"",
+            indexLabel: "{label} {y}",
+            dataPoints: [
+                {y: 79.45, label: "Google"},
+                {y: 7.31, label: "Bing"},
+                {y: 7.06, label: "Baidu"},
+                {y: 4.91, label: "Yahoo"},
+                {y: 1.26, label: "Others"}
+            ]
+        }],
+        backgroundColor: "#e8e8e8",
+        height: 258 
+    });
+    chart.render();
 }
 
 var dollarString = function(num) {
