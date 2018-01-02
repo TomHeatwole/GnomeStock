941248018bdbb21f515e1c4b18734ca04e458eb2
tomheatwole <tgh8xn@virginia.edu>
2018-01-02 14:05:30 -0500

    Weekly evaluations accepted on /mobile

--- public/mobile/mobile.js
+++ public/mobile/mobile.js
@@ -1,6 +1,7 @@
 var masterData;
 var weekIndex;
 var monthIndex;
+var wait = false;
 
 $(document).ready(function() {
     firebase.database().ref("master").once("value").then(function(master) {
@@ -30,6 +31,8 @@ var displayLoadedPage = function() {
 }
 
 var submitDaily = function() {
+    if (wait) return;
+    wait = true;
     var rating = document.getElementById("dailyRating").value;
     var pin = document.getElementById("dailyPin").value;
     var foundPin = false;
@@ -44,11 +47,66 @@ var submitDaily = function() {
                             alert("You have successfully entered your daily rating");
                             home();
                         });
-                    } else
+                    } else {
                         document.getElementById("error").style = "display: lol; color: red";
+                        wait = false;
+                    }
                 }
                 count++;
-                if (count === 4 && !foundPin) alert("Pin does not match any user");
+                if (count === 4 && !foundPin) {
+                    alert("Pin does not match any user");
+                    wait = false;
+                }
+            });
+        })(u);
+    });
+}
+
+var submitWeekly = function() {
+    if (wait) return;
+    wait = true;
+    var rating = document.getElementById("weeklyRating").value;
+    var pin = document.getElementById("weeklyPin").value;
+    var foundPin = false;
+    var count = 0;
+    firebase.database().ref("user").once("value").then(function(users) {
+        for (var u in users.val()) (function(u) {
+            firebase.database().ref("user/" + u).once("value").then(function(usr) {
+                if (usr.val().pin === pin) {
+                    foundPin = true;
+                    var weekCount = 0;
+                    var cont = true;
+                    for (var w in usr.val().weekly) weekCount++;
+                    for (var w in usr.val().weekly) (function(w) {
+                        firebase.database().ref("user/" + u + "/weekly/" + w).once("value").then(function(week) {
+                            if (week.val().week === weekIndex) {
+                                cont = false;
+                                alert("You already entered a weekly evaluation for this week");
+                                home();
+                            }
+                        });
+                        weekCount--;
+                        if (weekCount === 0 && cont) {
+                            if (!isNaN(rating) && isFinite(rating) && rating >= 0 && rating <= 100 && rating !== "") {
+                                firebase.database().ref("user/" + u + "/weekly").push({
+                                    week: weekIndex,
+                                    rating: parseInt(rating)
+                                }).then(function() {
+                                    alert("You have successfully entered your weekly rating");
+                                    home();
+                                });
+                            } else {
+                                wait = false;
+                                document.getElementById("error").style = "display: lol; color: red";
+                            }
+                        }
+                    })(w);
+                }
+                count++;
+                if (count === 4 && !foundPin) {
+                    wait = false;
+                    alert("Pin does not match any user");
+                }
             });
         })(u);
     });
